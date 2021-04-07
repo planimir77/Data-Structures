0615e54479ec962e4e9def04c4a43389663fc164
planimir77 <planimir7708@gmail.com>
2021-04-07 21:18:38 +0300

    initial CombinedExercises

---
+++ Algorithm-Complexity-and-Linear-Data-Structures-Exercise/SumAndAverage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace SumAndAverage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
