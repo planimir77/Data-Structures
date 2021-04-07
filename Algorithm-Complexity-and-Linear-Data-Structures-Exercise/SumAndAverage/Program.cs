5aaf30c498dcc3752d2c8bcc9b1b0db22a89435c
planimir77 <planimir7708@gmail.com>
2021-04-07 21:21:10 +0300

    solved SumAndAverage

--- Algorithm-Complexity-and-Linear-Data-Structures-Exercise/SumAndAverage/Program.cs
+++ Algorithm-Complexity-and-Linear-Data-Structures-Exercise/SumAndAverage/Program.cs
@@ -7,7 +7,11 @@ namespace SumAndAverage
     {
         static void Main(string[] args)
         {
+            var sequence = Console.ReadLine()?.Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            var sum = sequence?.Sum();
+            var average = sum == 0 ? 0 : (double)sum / sequence.Count;
             
+            Console.WriteLine($"Sum={sum}; Average={average:F2}");
         }
     }
 }
