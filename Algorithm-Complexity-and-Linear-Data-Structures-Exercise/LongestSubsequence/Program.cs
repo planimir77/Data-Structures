2c32e034af3dfde8118acf269090da052df14f7f
planimir77 <planimir7708@gmail.com>
2021-04-08 13:43:17 +0300

    solved LongestSubsequence

--- Algorithm-Complexity-and-Linear-Data-Structures-Exercise/LongestSubsequence/Program.cs
+++ Algorithm-Complexity-and-Linear-Data-Structures-Exercise/LongestSubsequence/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LongestSubsequence
 {
@@ -6,7 +8,51 @@ namespace LongestSubsequence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return;
+            }
+            var sequence = input.Split().Select(int.Parse).ToList();
+
+            var result = new List<int>();
+            var currentList = new List<int>();
+
+            for (int i = 0; i < sequence?.Count; i++)
+            {
+                var currentNumber = sequence[i];
+
+                if (i == 0)
+                {
+                    result.Add(currentNumber);
+                    currentList.Add(currentNumber);
+                    continue;
+                }
+
+                var previousNumber = sequence[i - 1];
+
+                if (currentNumber == previousNumber)
+                {
+                    currentList.Add(currentNumber);
+                }
+                else
+                {
+                    
+                    currentList.Clear();
+                    currentList.Add(currentNumber);
+                }
+
+                if (currentList.Count > result.Count)
+                {
+                    result.Clear();
+                    foreach (var number in currentList)
+                    {
+                        result.Add(number);
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", result));
         }
     }
 }
