fd01e8691be0a88ad6dec6e6b284203d8a21c714
planimir77 <planimir7708@gmail.com>
2021-04-09 15:45:11 +0300

    export in class

---
+++ Algorithm-Complexity-and-Linear-Data-Structures-Exercise/LongestSubsequence/LongestSubsequence.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LongestSubsequence
+{
+    public class LongestSubsequence
+    {
+        public string Get(string input)
+        {
+            var sequence = input.Split().Select(int.Parse).ToList();
+
+            var result = new List<int>();
+            var currentList = new List<int>();
+
+            for (int i = 0; i < sequence?.Count; i++)
+            {
+                var currentNumber = sequence[i];
+
+                if (i == 0)
+                {
+                    result.Add(currentNumber);
+                    currentList.Add(currentNumber);
+                    continue;
+                }
+
+                var previousNumber = sequence[i - 1];
+
+                if (currentNumber == previousNumber)
+                {
+                    currentList.Add(currentNumber);
+                }
+                else
+                {
+
+                    currentList.Clear();
+                    currentList.Add(currentNumber);
+                }
+
+                if (currentList.Count > result.Count)
+                {
+                    result.Clear();
+                    foreach (var number in currentList)
+                    {
+                        result.Add(number);
+                    }
+                }
+            }
+
+            return String.Join(" ", result);
+        }
+    }
+}
--- Algorithm-Complexity-and-Linear-Data-Structures-Exercise/LongestSubsequence/Program.cs
+++ Algorithm-Complexity-and-Linear-Data-Structures-Exercise/LongestSubsequence/Program.cs
@@ -8,51 +8,11 @@ namespace LongestSubsequence
     {
         static void Main(string[] args)
         {
+            var longestSubsequence = new LongestSubsequence();
+
             var input = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return;
-            }
-            var sequence = input.Split().Select(int.Parse).ToList();
 
-            var result = new List<int>();
-            var currentList = new List<int>();
-
-            for (int i = 0; i < sequence?.Count; i++)
-            {
-                var currentNumber = sequence[i];
-
-                if (i == 0)
-                {
-                    result.Add(currentNumber);
-                    currentList.Add(currentNumber);
-                    continue;
-                }
-
-                var previousNumber = sequence[i - 1];
-
-                if (currentNumber == previousNumber)
-                {
-                    currentList.Add(currentNumber);
-                }
-                else
-                {
-                    
-                    currentList.Clear();
-                    currentList.Add(currentNumber);
-                }
-
-                if (currentList.Count > result.Count)
-                {
-                    result.Clear();
-                    foreach (var number in currentList)
-                    {
-                        result.Add(number);
-                    }
-                }
-            }
-
-            Console.WriteLine(String.Join(" ", result));
+            Console.WriteLine(longestSubsequence.Get(input));
         }
     }
 }
