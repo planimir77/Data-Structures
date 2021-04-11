fb263c438a353401fb9e33fa1859279bf2a4496e
planimir77 <planimir7708@gmail.com>
2021-04-11 12:54:13 +0300

    done RemoveOddOccurrences

---
+++ Algorithm-Complexity-and-Linear-Data-Structures-Exercise/RemoveOddOccurrences/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RemoveOddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var removeOddOccurrences = new RemoveOddOccurrences();
+            Console.WriteLine(removeOddOccurrences.GetEven(Console.ReadLine()));
+        }
+    }
+}
---
+++ Algorithm-Complexity-and-Linear-Data-Structures-Exercise/RemoveOddOccurrences/RemoveOddOccurrences.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoveOddOccurrences
+{
+    public class RemoveOddOccurrences
+    {
+        public string GetEven(string input)
+        {
+            var sequence = input.Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < sequence.Count(); i++)
+            {
+                if (sequence.Count(x => x == sequence[i]) % 2 != 0)
+                {
+                    var target = sequence[i];
+                    sequence.RemoveAll(x => x == target);
+                    i--;
+                }
+            }
+
+            return String.Join(" ", sequence);
+        }
+    }
+}
